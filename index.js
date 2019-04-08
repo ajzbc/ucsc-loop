6606bec8c4118641c655a5f7e7bdc0baf8bfdd2f
ajzbc <contact.ajzbc@gmail.com>
2019-04-08 16:45:51 -0700

    updated key

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 mapkit.init({
-    authorizationCallback: function(done) {
-        done('eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1SWVBEV1dINzMifQ.eyJpc3MiOiJHWjg0NVlRUDZFIiwiaWF0IjoxNTUwODg4MzgzLCJleHAiOjE1NTM1NjY3ODN9.IQjTmTuPdcUbAc1cHBdNxIxOOmjgzrR42CdARBAcQoViW_DXuyDTWSH_U6eYkQFXfPlEFBFhriDL9ffehuxQNA');
+    authorizationCallback: function (done) {
+        done('eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkFERkMzRks5WUwifQ.eyJpc3MiOiJHWjg0NVlRUDZFIiwiaWF0IjoxNTU0NzY2ODAxLCJleHAiOjE1ODY5MDc2MDF9.PO_7FkHPWAH27l7u8brP4PUT5EBUPvQM9g8AIzn0wd8s0-RbFYMvRyuV7kdDWvKRXC_m_X4CJO82TZGbLmuxbw');
     }
 });
 
 var Campus = new mapkit.CoordinateRegion(
     new mapkit.Coordinate(36.989537530470294, -122.05852024725598),
     new mapkit.CoordinateSpan(0.0243376876410224, 0.022487640380859375)
-    );
+);
 
 var map = new mapkit.Map("map");
 
@@ -38,23 +38,21 @@ function placeBuses() {
         return response.json();
     }).then(data => {
 
-        var customA = function(coordinate, options) {
+        var customA = function (coordinate, options) {
             var div = document.createElement("div");
             div.className = "annotation";
 
             var emoji = document.createElement("p");
-            if(browser) {
+            if (browser) {
                 emoji.textContent = "\xa0🚌";
-            }
-            else {
+            } else {
                 emoji.textContent = "🚌";
             }
-            
 
-            if(options.data.color == true) {
+
+            if (options.data.color == true) {
                 emoji.className = "annotation-bad";
-            }
-            else {
+            } else {
                 emoji.className = "annotation-good";
             }
 
@@ -65,23 +63,25 @@ function placeBuses() {
 
         busLandmarks = [];
         map.removeAnnotations(map.annotations);
-        
-        data.forEach(function(bus) {
+
+        data.forEach(function (bus) {
             console.log(bus);
 
             var options = {
-                data: { color: bus.type.includes("OUT") }
+                data: {
+                    color: bus.type.includes("OUT")
+                }
             };
             var annotation = new mapkit.Annotation(new mapkit.Coordinate(bus.lat, bus.lon), customA, options);
             map.addAnnotation(annotation);
         });
 
-        if(!data[0]) {
+        if (!data[0]) {
             document.getElementById("service").innerHTML = 0;
         } else {
             document.getElementById("service").innerHTML = data.length;
         }
-    
+
     }).catch(err => {
         console.log(err);
     });
@@ -90,6 +90,6 @@ function placeBuses() {
 placeBuses();
 map.region = Campus;
 
-window.setInterval(function(){
+window.setInterval(function () {
     placeBuses();
 }, 3000);
